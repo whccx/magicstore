ef04226865005ffd43f0eceeacde0a9702bd4f25
hll <dream.hll@foxmail.com>
2019-09-18 16:54:46 +0800

    hll-eight

--- magic/apps/user/models.py
+++ magic/apps/user/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class VerifyCode(models.Model):
+    '''
+    短信验证码
+    '''
+    code = models.CharField(max_length=20,verbose_name='验证码',help_text='验证码')
+    mobile = models.CharField(max_length=11,verbose_name='电话',help_text='电话')
+    add_time = models.DateTimeField(auto_now_add=True,verbose_name='添加时间')
+
+    class Meta:
+        verbose_name_plural = verbose_name = '短信验证码'
+
+    def __str__(self):
+        return self.code
