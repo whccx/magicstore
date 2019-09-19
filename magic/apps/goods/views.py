1847859c9cb58e1c5d46a433d82cac56e03c3c3b
whccx <whccx@qq.com>
2019-09-19 15:48:14 +0800

    goods分权限---ccx

--- magic/apps/goods/views.py
+++ magic/apps/goods/views.py
@@ -1,22 +1,17 @@
 # -*- coding: utf-8 -*-
 from rest_framework.response import Response
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework import viewsets,mixins,status
-from .serializers import GoodsSerializer,AdminGoodsSerializer
+from .serializers import GoodsSerializer
 from goods.models import GoodsBase
 
 # 商品信息
-class GoodsViewSet(viewsets.ReadOnlyModelViewSet,viewsets.GenericViewSet):
+class GoodsViewSet(viewsets.ModelViewSet,viewsets.GenericViewSet):
+    # 认证
+    authentication_classes = (JSONWebTokenAuthentication,)
+    # 权限
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
     queryset = GoodsBase.objects.all()
     serializer_class = GoodsSerializer
-
-# 后台商品
-class AdminGoodsViewSet(viewsets.ModelViewSet,viewsets.GenericViewSet):
-    # 认证
-    #authentication_classes = (JSONWebTokenAuthentication,)
-    # 权限
-    #permission_classes = (IsAuthenticated,)
-
-    queryset = GoodsBase.objects.all()
-    serializer_class = AdminGoodsSerializer
--- magic/magic/urls.py
+++ magic/magic/urls.py
@@ -10,26 +10,34 @@ from rest_framework_jwt.views import obtain_jwt_token,refresh_jwt_token
 from django.views.generic.base import RedirectView
 from rest_framework_jwt.views import obtain_jwt_token
 from user.views import UsersViewSet
-from goods.views import GoodsViewSet,AdminGoodsViewSet
+from goods.views import GoodsViewSet
 
 router = routers.DefaultRouter()
 
 #用户注册
 router.register(r'users', UsersViewSet,base_name='users')
 router.register(r'goods', GoodsViewSet,base_name='goods')
-router.register(r'admin_goods', AdminGoodsViewSet,base_name='admin_goods')
 
 
 urlpatterns = [
+    #Django后台
     path('admin/', admin.site.urls),
+
+    #浏览器图标
     path('favicon.ico', RedirectView.as_view(url='static/favicon.ico')),
+
+    #rest登录
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
+    #签发token
     path('login/', obtain_jwt_token),
+    #刷新token
     path('refresh/', refresh_jwt_token),
 
+    #API路由
     path('', include(router.urls)),
 
+    #富文本
     path('ueditor/',include('DjangoUeditor.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
